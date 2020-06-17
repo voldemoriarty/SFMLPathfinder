5347ae79c431e2345a71d012919ee0df4189c40a
SmartGridsML <bilal.saleem_96@outlook.com>
2020-06-17 12:59:30 +0100

    Blue window

--- sfmlproj/main.cpp
+++ sfmlproj/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML now works!");
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
+    shape.setFillColor(sf::Color::Blue);
 
     while (window.isOpen())
     {
