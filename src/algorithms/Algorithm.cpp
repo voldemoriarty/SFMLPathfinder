a30035db41c9563c04fea759bb0482ac4d4cc660
M Saad Bin Amin <mamin.bee16seecs@seecs.edu.pk>
2020-06-30 23:02:18 +0500

    time runComplete

--- src/algorithms/Algorithm.cpp
+++ src/algorithms/Algorithm.cpp
@@ -3,6 +3,18 @@
 //
 
 #include "Algorithm.h"
+#include <chrono>
+
+
+// too big to type
+inline auto now() {
+    return std::chrono::high_resolution_clock::now();
+}
+
+template <class Time>
+inline auto microseconds(Time then, Time now) {
+    return std::chrono::duration_cast<std::chrono::microseconds>(now - then).count();
+}
 
 bool Algorithm::checkGrid(GridPanel &grid) {
     grid.clearAllPaths();
@@ -13,11 +25,15 @@ bool Algorithm::checkGrid(GridPanel &grid) {
 }
 
 bool Algorithm::runComplete(GridPanel &grid) {
+    auto then = now();
     if (!init(grid))
         return false;
 
     while (!step(grid));
-    return postRun(grid);
+    bool ret = postRun(grid);
+    timeUs = microseconds(then, now());
+
+    return ret;
 }
 
 bool Algorithm::step(GridPanel &grid) {
@@ -27,6 +43,7 @@ bool Algorithm::step(GridPanel &grid) {
 
 bool Algorithm::init(GridPanel &grid) {
     tiles = 0;
+    timeUs = 0;
     return preRun(grid);
 }
 
